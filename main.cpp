4089c32cdd82329dbc05e1fbb8f5ccb9bda63fb6
Walter Aoiama Nagai <walternagai@unifei.edu.br>
2021-06-21 21:35:43 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,67 +1,49 @@
 #include <iostream>
 using namespace std;
 
-/*
-  MDC (a, b)
-  Máximo Divisor Comum entre dois números
-*/
-int MDC (int a, int b) {
-  /*
-  Algoritmo de Euclides
+const int QTDE_NOTAS = 5;
 
-  enquanto b diferente de zero faça
-    r recebe resto da divisão entre a e b
-    a recebe o valor de b
-    b recebe o valor de r
-  fim-enquanto
-  retorne a
-  */
-  return 0;
+/* 
+  leNotas (notas)
+  Realiza a leitura de notas que podem variar de 0 até 10.
+
+  notas: vetor de notas de tamanho QTDE_NOTAS
+*/
+void leNotas (float notas[QTDE_NOTAS]) {
+  for (int i = 0; i < QTDE_NOTAS; i++) {
+    cin >> notas[i];
+    while (notas[i] < 0 || notas[i] > 10) {
+      cin >> notas[i];
+    } // fim while
+  } // fim for
 }
 
 /*
-  MMC (a, b)
-  Mínimo Múltiplo Comum entre dois números
-*/
-int MMC (int a, int b) {
-  /*
-    MMC(a, b) * MDC(a, b) = a * b
+  calculaMedia (notas) : float
+  Calcula a média das notas, desconsiderando a menor nota.
 
-    Para calcular o MMC, pode-se usar uma das fórmulas:
-    a) MMC(a, b) = a * (b / MDC(a, b)) OU
-    b) MMC(a, b) = b * (a / MDC(a, b))
-  */
-  return 0;
-}
-
-/*
-  saoPrimos (a, b)
-  Dois números são primos se o MDC é igual a 1
+  notas: vetor de notas de tamanho QTDE_NOTAS
 */
-bool saoPrimos (int a, int b) {
-  /*
-    se o MDC (a, b) é igual a 1 retorne true
-    caso contrário retorne falso
-  */    
-  return false;
+float calculaMedia (float notas[QTDE_NOTAS]) {
+  float menor, soma;
+  soma  = 0;
+  menor = notas[0];
+  soma  = soma + notas[0];
+  for (int i = 1; i < QTDE_NOTAS; i++) {
+    if (notas[i] < menor)
+      menor = notas [i];
+    soma = soma + notas [i];
+  } //fim for
+  soma = soma - menor;
+  return soma / (QTDE_NOTAS-1);
 }
 
 int main() {
-  int numA, numB;
+  float notas [QTDE_NOTAS];
 
-  cin >> numA >> numB;
+  leNotas (notas);
 
-  cout << "MDC(" << numA << ", " << numB << ") = "
-       << MDC (numA, numB) << endl;
-  cout << "MMC(" << numA << ", " << numB << ") = "
-       << MMC (numA, numB) << endl;
-  
-  cout << numA << " e " << numB;
-  bool primos = saoPrimos(numA, numB);
-  if (primos == true)
-    cout << " sao primos.\n";
-  else
-    cout << " não sao primos.\n";
+  cout << "Media = " << calculaMedia (notas) << endl;
 
   return 0;
 }
